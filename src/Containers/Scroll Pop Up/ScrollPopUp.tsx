4672c9558ef51b5bbb3c929747447bf18a0386c8
Aryansh Mahato <aryanshmahato@gmail.com>
2020-03-06 21:49:28 +0530

    Documents Scroll Pop Up

--- src/Containers/Scroll Pop Up/ScrollPopUp.tsx
+++ src/Containers/Scroll Pop Up/ScrollPopUp.tsx
@@ -1,34 +1,35 @@
-import React, { useState } from "react";
-import { FaChevronDown } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaChevronDown } from 'react-icons/fa';
 
-import styles from "./ScrollPopUp.module.scss";
+import styles from './ScrollPopUp.module.scss';
 
+// Prop Types
 interface ScrollPopUpProps {
-  didScrolled: boolean;
+	didScrolled: boolean;
 }
 
+// Export Function
 const ScrollPopUp = ({ didScrolled }: ScrollPopUpProps) => {
-  const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
+	let classes = [styles.scrollPopUp];
 
-  let classes = [styles.scrollPopUp];
+	// set ShowPopUp After 5s
+	setTimeout(() => setShow(true), 5000);
 
-  // set ShowPopUp After 5s
-  setTimeout(() => setShow(true), 5000);
+	if (didScrolled) {
+		classes = [styles.scrollPopUp];
+	}
 
-  if (didScrolled) {
-    classes = [styles.scrollPopUp];
-  }
+	// If user haven't scrolled, and show is true, Popup will be shown
+	if (show && !didScrolled) {
+		classes = [styles.scrollPopUp, styles.show];
+	}
 
-  // If user haven't scrolled, and show is true, Popup will be shown
-  if (show && !didScrolled) {
-    classes = [styles.scrollPopUp, styles.show];
-  }
-
-  return (
-    <div className={classes.join(" ")}>
-      Scroll <FaChevronDown />
-    </div>
-  );
+	return (
+		<div className={classes.join(' ')}>
+			Scroll <FaChevronDown />
+		</div>
+	);
 };
 
 export default ScrollPopUp;

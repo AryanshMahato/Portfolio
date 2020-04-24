3fbf405fd316feb2203dd6a712fed5d30f2afd48
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-25 00:52:06 +0530

    Hover Buttons Added

--- src/Components/Project/Project.tsx
+++ src/Components/Project/Project.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Project.module.scss";
+import GithubLink from "../ProjectLinks/GithubLink";
+import LiveLink from "../ProjectLinks/LiveLink";
 
 interface ProjectProps {
   project: {
@@ -11,10 +13,32 @@ interface ProjectProps {
 }
 
 export default ({ project }: ProjectProps) => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const hoverStarted = () => {
+    setShowLinks(true);
+  };
+
+  const hoverEnded = () => {
+    setShowLinks(false);
+  };
+
+  let buttonClass = showLinks
+    ? classes.buttons + " " + classes.show
+    : classes.buttons;
+
   return (
     <div className={classes.root}>
-      <div className={classes.imageContainer}>
+      <div
+        className={classes.imageContainer}
+        onMouseEnter={hoverStarted}
+        onMouseLeave={hoverEnded}
+      >
         <img src={project.image} alt={project.name} className={classes.image} />
+        <div className={buttonClass}>
+          <GithubLink githubLink={project.githubLink} />
+          <LiveLink liveLink={project.liveLink!} />
+        </div>
       </div>
       <p className={classes.name}>{project.name}</p>
     </div>

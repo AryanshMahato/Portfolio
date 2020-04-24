dd2a745511d5ef77e2b276aa862714d47018cd3f
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-24 23:47:42 +0530

    Projects Annotation Added

--- src/Containers/Projects/Projects.tsx
+++ src/Containers/Projects/Projects.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import classes from "./Projects.module.scss";
+import Heading from "../../Global/Heading/Heading";
 
 export default () => {
-  return <div className={classes.root}></div>;
+  return (
+    <div className={classes.root}>
+      <Heading>PROJECTS</Heading>
+      <a
+        href={"https://github.com/AryanshMahato"}
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Checkout More Projects on my Github
+      </a>
+    </div>
+  );
 };

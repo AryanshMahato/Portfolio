eecd78278ec2058bfcbbea5bb8ecdd289e0eedfe
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-24 23:28:29 +0530

    Projects BoilerPlate Added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import HeroBox from "./Components/HeroBox/HeroBox";
 import ParticleBackground from "./Components/Particle Background/ParticleBackground";
 import Skills from "./Components/Skills/Skills";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax/cjs";
+import Projects from "./Containers/Projects/Projects";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <HeroBox />
       </Parallax>
       <Skills />
+      <Projects />
       <div className="test" />
     </ParallaxProvider>
   );
---
+++ src/Containers/Projects/Projects.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./Projects.module.scss";
+
+export default () => {
+  return <div className={classes.root}></div>;
+};

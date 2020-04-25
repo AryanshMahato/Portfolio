b54b9360da3e79e0b2b7ae90c882512ea2552051
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-26 03:21:13 +0530

    Native Parallax Added

--- src/Components/HeroBox/HeroBox.tsx
+++ src/Components/HeroBox/HeroBox.tsx
@@ -3,6 +3,8 @@ import styles from "./HeroBox.module.scss";
 import ScrollPopUp from "../../Containers/Scroll Pop Up/ScrollPopUp";
 
 const HeroBox = () => {
+  let i = 0;
+  let lastOffset = 0;
   const [didScrolled, setScrolled] = useState(false);
 
   let isFirstRender = true;
@@ -15,9 +17,36 @@ const HeroBox = () => {
     return window.removeEventListener("scroll", () => setScrolled(false));
   }, [didScrolled]);
 
+  window.addEventListener("scroll", () => {
+    if (i * 5 < 200) {
+      if (lastOffset < window.pageYOffset)
+        // @ts-ignore
+        document.getElementById("heroBox").style.transform = `translateY(${
+          ++i * 5
+        }px)`;
+    }
+
+    if (lastOffset > window.pageYOffset) {
+      if (i > 0) {
+        // @ts-ignore
+        document.getElementById("heroBox").style.transform = `translateY(${
+          --i * 5
+        }px)`;
+      }
+      if (window.pageYOffset === 0) {
+        // @ts-ignore
+        document.getElementById(
+          "heroBox"
+        ).style.transform = `translateY(${0}px)`;
+      }
+    }
+
+    lastOffset = window.pageYOffset;
+  });
+
   return (
     <div className={styles.herBoxContainer}>
-      <div className={styles.herBox}>
+      <div className={styles.herBox} id={"heroBox"}>
         <div className={styles.title}>
           Hello, I am <span>Aryansh Mahato</span>
         </div>

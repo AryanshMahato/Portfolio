7786fc776b1a16b68000dafdd94d0fbc9dbca15a
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-25 23:25:15 +0530

    New Data Added and Meta Changes

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import styles from "./Skills.module.scss";
 import SkillsList from "../../Containers/Skills List/SkillsList";
-import ReadMoreButton from "../../Global/Buttons/ReadMoreButton";
 import Heading from "../../Global/Heading/Heading";
 
 export default function Skills() {
-  const readMoreClicked = () => {
-    console.log("Readmore Clicked");
-  };
-
   return (
     <div className={styles.Skills} id={"skills"}>
       <Heading>Skills</Heading>
       <div className={styles.SkillsListContainer}>
         <SkillsList />
-        <div className={styles.button}>
-          <ReadMoreButton onClick={readMoreClicked} />
-        </div>
       </div>
     </div>
   );
--- src/Data/Skills.ts
+++ src/Data/Skills.ts
@@ -2,14 +2,14 @@ import html from "../Assets/html5-brands.svg";
 import css from "../Assets/css.svg";
 import javascript from "../Assets/javascript.svg";
 import react from "../Assets/react.svg";
-import redux from "../Assets/redux.svg";
 import typescript from "../Assets/typescript.svg";
+import node from "../Assets/node.png";
 
 export default [
   { name: "HTML", image: html },
   { name: "CSS", image: css },
   { name: "JavaScript", image: javascript },
   { name: "React", image: react },
-  { name: "Redux", image: redux },
   { name: "TypeScript", image: typescript },
+  { name: "Node JS", image: node },
 ];

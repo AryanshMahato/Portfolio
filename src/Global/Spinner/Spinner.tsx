0d5b4ad68d3e14d9b39e3466031d27c729f64ba3
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-25 22:11:30 +0530

    Scroll Lock Added During Spinner

--- src/Global/Spinner/Spinner.tsx
+++ src/Global/Spinner/Spinner.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Spinner.module.scss";
 import Backdrop from "../Backdrop/Backdrop";
 
 export default () => {
+  useEffect(() => {
+    // @ts-ignore
+    document.querySelector("body").style.overflow = "hidden";
+    return () => {
+      // @ts-ignore
+      document.querySelector("body").style.overflowY = "scroll";
+    };
+  }, []);
+
   return (
     <div className={classes.root}>
       <Backdrop />

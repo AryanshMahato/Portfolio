64884f8bf81de305d1215bc0a535968d5e6d89c4
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-26 01:45:54 +0530

    Parallax Removed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import NavBar from "./Components/NavBar/NavBar";
 import HeroBox from "./Components/HeroBox/HeroBox";
 import ParticleBackground from "./Components/Particle Background/ParticleBackground";
 import Skills from "./Components/Skills/Skills";
-import { Parallax, ParallaxProvider } from "react-scroll-parallax/cjs";
 import Projects from "./Containers/Projects/Projects";
 import Background from "./Global/Background/Background";
 import ContactUs from "./Containers/Contact Us/ContactUs";
@@ -14,12 +13,10 @@ import About from "./Containers/About/About";
 function App() {
   console.error("Please Refresh the browser while checking responsiveness");
   return (
-    <ParallaxProvider>
-      <Parallax y={[-40, 40]} tagOuter="figure">
-        <ParticleBackground />
-        <NavBar />
-        <HeroBox />
-      </Parallax>
+    <>
+      <ParticleBackground />
+      <NavBar />
+      <HeroBox />
       <Background>
         <About />
         <Skills />
@@ -27,7 +24,7 @@ function App() {
       </Background>
       <ContactUs />
       <Footer />
-    </ParallaxProvider>
+    </>
   );
 }
 

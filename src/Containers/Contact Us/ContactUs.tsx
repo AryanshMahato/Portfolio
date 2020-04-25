a754f73c48bf88018262866c10e2f09ff1185f5f
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-25 21:46:17 +0530

    Email Js Added

--- src/Containers/Contact Us/ContactUs.tsx
+++ src/Containers/Contact Us/ContactUs.tsx
@@ -3,11 +3,32 @@ import classes from "./ContactUs.module.scss";
 import Undraw from "../../Assets/Undraw.svg";
 import ContactUsForm from "../../Components/Forms/ContactUsForm";
 import Heading from "../../Global/Heading/Heading";
+import * as emailjs from "emailjs-com";
 
 export default () => {
   const formSubmitHandler = (e: FormEvent<HTMLFormElement>, values: any) => {
     e.preventDefault();
-    console.log(values);
+    let templateParams = {
+      from_name: values.email,
+      to_name: "aryanshdev@gmail.com",
+      subject: `${values.name} saw your Portfolio and wanted to have a chat`,
+      message_html: values.message,
+    };
+
+    console.log(
+      process.env.REACT_APP_TEMPLATE_ID,
+      process.env.REACT_APP_USER_ID
+    );
+
+    emailjs
+      .send(
+        "gmail",
+        process.env.REACT_APP_TEMPLATE_ID!,
+        templateParams,
+        process.env.REACT_APP_USER_ID!
+      )
+      .then((e) => console.log(e))
+      .catch((e) => console.error(e));
   };
 
   return (
